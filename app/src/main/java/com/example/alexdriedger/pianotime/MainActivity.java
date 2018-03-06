42b6fe05442d34b414736a34002ef48ae89e61db
alexdriedger <aldriedger@gmail.com>
2018-03-06 14:25:59 -0800

    MIDI hello world

--- app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
+++ app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
@@ -2,12 +2,77 @@ package com.example.alexdriedger.pianotime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.billthefarmer.mididriver.MidiDriver;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MidiDriver midiDriver;
+    private byte[] event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playNote();
+            }
+        });
+
+        final Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopNote();
+            }
+        });
+
+        // Instantiate the driver.
+        midiDriver = new MidiDriver();
+
+        midiDriver.start();
+
+        // Get the configuration.
+//        config = midiDriver.config();
+    }
+
+    private void playNote() {
+
+        // Construct a note ON message for the middle C at maximum velocity on channel 1:
+        event = new byte[3];
+        event[0] = (byte) (0x90 | 0x00);  // 0x90 = note On, 0x00 = channel 1
+        event[1] = (byte) 0x3C;  // 0x3C = middle C
+        event[2] = (byte) 0x7F;  // 0x7F = the maximum velocity (127)
+
+        // Internally this just calls write() and can be considered obsoleted:
+        //midiDriver.queueEvent(event);
+
+        // Send the MIDI event to the synthesizer.
+        midiDriver.write(event);
+
+        Log.d("Main", "playNote");
+
+    }
+
+    private void stopNote() {
+
+        // Construct a note OFF message for the middle C at minimum velocity on channel 1:
+        event = new byte[3];
+        event[0] = (byte) (0x80 | 0x00);  // 0x80 = note Off, 0x00 = channel 1
+        event[1] = (byte) 0x3C;  // 0x3C = middle C
+        event[2] = (byte) 0x00;  // 0x00 = the minimum velocity (0)
+
+        // Send the MIDI event to the synthesizer.
+        midiDriver.write(event);
+
+        Log.d("Main", "pauseNote");
+
     }
 }
