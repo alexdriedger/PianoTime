18ed50fc78fb27706c5b8b57b4475ee4dd11d101
Alex Driedger <aldriedger@gmail.com>
2018-03-08 20:49:40 -0800

    Alex/midi player (#2)
    * Added midi files
    
    * Changing instruments

--- app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
+++ app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.alexdriedger.pianotime;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
 import org.billthefarmer.mididriver.MidiDriver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+
 public class MainActivity extends AppCompatActivity {
 
     private MidiDriver midiDriver;
     private byte[] event;
+    private int[] config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +27,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button playButton = findViewById(R.id.play_button);
-        playButton.setOnClickListener(new View.OnClickListener() {
+        playButton.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                playNote();
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    playNote((byte) 0x01);
+                }
+                else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    stopNote((byte) 0x01);
+                }
+                return true;
             }
         });
 
-        final Button pauseButton = findViewById(R.id.pause_button);
-        pauseButton.setOnClickListener(new View.OnClickListener() {
+        final Button playButton2 = findViewById(R.id.play_button_2);
+        playButton2.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                stopNote();
+            public boolean onTouch(View view, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    playNote((byte) 0x00);
+                }
+                else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    stopNote((byte) 0x00);
+                }
+                return true;
             }
         });
+
+//        final Button pauseButton = findViewById(R.id.pause_button);
+//        pauseButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                stopNote();
+//            }
+//        });
 
         // Instantiate the driver.
         midiDriver = new MidiDriver();
@@ -40,19 +68,29 @@ public class MainActivity extends AppCompatActivity {
         midiDriver.start();
 
         // Get the configuration.
-//        config = midiDriver.config();
+        config = midiDriver.config();
+
+        // Print out the details.
+        Log.d(this.getClass().getName(), "maxVoices: " + config[0]);
+        Log.d(this.getClass().getName(), "numChannels: " + config[1]);
+        Log.d(this.getClass().getName(), "sampleRate: " + config[2]);
+        Log.d(this.getClass().getName(), "mixBufferSize: " + config[3]);
+
+        changeInstrument((byte) 0x5B, (byte) 0x01);
+
+//        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.moana_midi);
+//        mediaPlayer.start();
+
     }
 
-    private void playNote() {
+    // private void playNote(byte chan, byte note, byte vel)
+    private void playNote(byte channel) {
 
         // Construct a note ON message for the middle C at maximum velocity on channel 1:
         event = new byte[3];
-        event[0] = (byte) (0x90 | 0x00);  // 0x90 = note On, 0x00 = channel 1
+        event[0] = (byte) (0x90 | channel);  // 0x90 = note On, 0x00 = channel 1
         event[1] = (byte) 0x3C;  // 0x3C = middle C
         event[2] = (byte) 0x7F;  // 0x7F = the maximum velocity (127)
-
-        // Internally this just calls write() and can be considered obsoleted:
-        //midiDriver.queueEvent(event);
 
         // Send the MIDI event to the synthesizer.
         midiDriver.write(event);
@@ -61,11 +99,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void stopNote() {
+    private void changeInstrument(byte instrument, byte channel) {
+        event = new byte[2];
+        event[0] = (byte) (0xC0 | channel);  // 0xC0 = change instrument, 0x00 = channel 1
+        event[1] = instrument;  // 0x3C = middle C // byte
+//        event[2] = (byte) 0x7F;  // 0x7F = the maximum velocity (127)
+
+        // Send the MIDI event to the synthesizer.
+        midiDriver.write(event);
+    }
+
+    // private void stopNote(byte chan, byte note)
+    private void stopNote(byte channel) {
 
         // Construct a note OFF message for the middle C at minimum velocity on channel 1:
         event = new byte[3];
-        event[0] = (byte) (0x80 | 0x00);  // 0x80 = note Off, 0x00 = channel 1
+        event[0] = (byte) (0x80 | channel);  // 0x80 = note Off, 0x00 = channel 1
         event[1] = (byte) 0x3C;  // 0x3C = middle C
         event[2] = (byte) 0x00;  // 0x00 = the minimum velocity (0)
 
