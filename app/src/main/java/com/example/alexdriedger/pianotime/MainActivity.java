5fb8fa58fe530e904a8df17063e4dcc66fa46a09
alexdriedger <aldriedger@gmail.com>
2018-03-15 19:23:18 -0700

    Midi encoding works

--- app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
+++ app/src/main/java/com/example/alexdriedger/pianotime/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alexdriedger.pianotime;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,12 +11,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import com.leff.midi.MidiFile;
+import com.leff.midi.MidiTrack;
+import com.leff.midi.event.meta.Tempo;
+import com.leff.midi.event.meta.TimeSignature;
+
 import org.billthefarmer.mididriver.MidiDriver;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,6 +56,57 @@ public class MainActivity extends AppCompatActivity {
                 changeActivity(MusicPlayerActivity.class);
             }
         });
+
+        // 1. Create some MidiTracks
+        MidiTrack tempoTrack = new MidiTrack();
+        MidiTrack noteTrack = new MidiTrack();
+
+// 2. Add events to the tracks
+// Track 0 is the tempo map
+        TimeSignature ts = new TimeSignature();
+        ts.setTimeSignature(4, 4, TimeSignature.DEFAULT_METER, TimeSignature.DEFAULT_DIVISION);
+
+        Tempo tempo = new Tempo();
+        tempo.setBpm(228);
+
+        tempoTrack.insertEvent(ts);
+        tempoTrack.insertEvent(tempo);
+
+// Track 1 will have some notes in it
+        final int NOTE_COUNT = 80;
+
+        for(int i = 0; i < NOTE_COUNT; i++)
+        {
+            int channel = 0;
+            int pitch = 1 + i;
+            int velocity = 100;
+            long tick = i * 480;
+            long duration = 120;
+
+            noteTrack.insertNote(channel, pitch, velocity, tick, duration);
+        }
+
+// 3. Create a MidiFile with the tracks we created
+        List<MidiTrack> tracks = new ArrayList<MidiTrack>();
+        tracks.add(tempoTrack);
+        tracks.add(noteTrack);
+
+        MidiFile midi = new MidiFile(MidiFile.DEFAULT_RESOLUTION, tracks);
+
+// 4. Write the MIDI data to a file
+        File output = new File(getApplicationContext().getFilesDir(), "exampleout.mid");
+        try
+        {
+            midi.writeToFile(output);
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
+            Log.d("midi", "Failed to write midi file");
+        }
+        
+        MediaPlayer mp = MediaPlayer.create(this, Uri.fromFile(output));
+        mp.start();
 
     }
 
