56d68c0af0ee87d2eba5b149e0ef4e7fb23b2768
Alex Driedger <aldriedger@gmail.com>
2018-04-03 18:32:54 -0700

    Real piano interface (#14)

--- app/src/main/java/com/example/alexdriedger/pianotime/KeyboardFragment.java
+++ app/src/main/java/com/example/alexdriedger/pianotime/KeyboardFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import com.example.alexdriedger.pianotime.MidiController;
 
 import org.billthefarmer.mididriver.MidiDriver;
@@ -29,29 +28,29 @@ public class KeyboardFragment extends AbstractMusicInteractionFragment {
      * Initializes keys for the touch screen piano
      */
     private void keyInit(View v) {
-        final Button mButtonC = v.findViewById(R.id.button_c);
+        final View mButtonC = v.findViewById(R.id.button_c);
         mButtonC.setOnTouchListener(new MusicInteractionOnTouchListener(0));
-        final Button mButtonCSharp = v.findViewById(R.id.button_c_sharp);
+        final View mButtonCSharp = v.findViewById(R.id.button_c_sharp);
         mButtonCSharp.setOnTouchListener(new MusicInteractionOnTouchListener(1));
-        final Button mButtonD = v.findViewById(R.id.button_d);
+        final View mButtonD = v.findViewById(R.id.button_d);
         mButtonD.setOnTouchListener(new MusicInteractionOnTouchListener(2));
-        final Button mButtonDSharp = v.findViewById(R.id.button_d_sharp);
+        final View mButtonDSharp = v.findViewById(R.id.button_d_sharp);
         mButtonDSharp.setOnTouchListener(new MusicInteractionOnTouchListener(3));
-        final Button mButtonE = v.findViewById(R.id.button_e);
+        final View mButtonE = v.findViewById(R.id.button_e);
         mButtonE.setOnTouchListener(new MusicInteractionOnTouchListener(4));
-        final Button mButtonF = v.findViewById(R.id.button_f);
+        final View mButtonF = v.findViewById(R.id.button_f);
         mButtonF.setOnTouchListener(new MusicInteractionOnTouchListener(5));
-        final Button mButtonFSharp = v.findViewById(R.id.button_f_sharp);
+        final View mButtonFSharp = v.findViewById(R.id.button_f_sharp);
         mButtonFSharp.setOnTouchListener(new MusicInteractionOnTouchListener(6));
-        final Button mButtonG = v.findViewById(R.id.button_g);
+        final View mButtonG = v.findViewById(R.id.button_g);
         mButtonG.setOnTouchListener(new MusicInteractionOnTouchListener(7));
-        final Button mButtonGSharp = v.findViewById(R.id.button_g_sharp);
+        final View mButtonGSharp = v.findViewById(R.id.button_g_sharp);
         mButtonGSharp.setOnTouchListener(new MusicInteractionOnTouchListener(8));
-        final Button mButtonA = v.findViewById(R.id.button_a);
+        final View mButtonA = v.findViewById(R.id.button_a);
         mButtonA.setOnTouchListener(new MusicInteractionOnTouchListener(9));
-        final Button mButtonASharp = v.findViewById(R.id.button_a_sharp);
+        final View mButtonASharp = v.findViewById(R.id.button_a_sharp);
         mButtonASharp.setOnTouchListener(new MusicInteractionOnTouchListener(10));
-        final Button mButtonB = v.findViewById(R.id.button_b);
+        final View mButtonB = v.findViewById(R.id.button_b);
         mButtonB.setOnTouchListener(new MusicInteractionOnTouchListener(11));
 
     }
